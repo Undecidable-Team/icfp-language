de397d6bdb5f62d509ba1f49cd5cd62cdc02f2c7
Mansur Ziiatdinov <gltronred@gmail.com>
2024-06-29 12:28:59 +0300

    Tests from spec (WIP)

--- test/Main.hs
+++ test/Main.hs
@@ -13,12 +13,20 @@ main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "Tests"
-  [ evalTests ]
+  [ evalTests
+  , parseTests
+  ]
+
+parseTests = testGroup "Parse" []
 
 evalTests = testGroup "Eval"
   [ strToIntTests
   , intToStrTests
-  , s2iRoundTests
+  -- , s2iRoundTests
+  , unaryTests
+  , binaryTests
+  , ifTests
+  , lambdaTests
   ]
 
 strToIntTests = testGroup "strToInt"
@@ -31,3 +39,35 @@ s2iRoundTests = testGroup "s2i roundtrip"
   [ testProperty "strToInt . intToStr == id" $
     \i -> strToInt (intToStr i) == i
   ]
+
+unaryTests = testGroup "Unary ops"
+  [ testCase "U- I$ -> -3" $
+    eval [] (VUnary OpNeg (VInt 3)) @?= VInt (-3)
+  , testCase "U! T -> false" $
+    eval [] (VUnary OpNot (VBool True)) @?= VBool False
+  , testCase "U# S4%34 -> 15818151" $
+    eval [] (VUnary OpStringToInt (VString "4%34")) @?= VInt 15818151
+
+    -- TODO: is this test correct?
+  , testCase "U$ I4%34 -> test" $
+    eval [] (VUnary OpIntToString (VInt 15818151)) @?= VString "4%34"
+  ]
+
+binaryTests = testGroup "Binary ops"
+  [ testCase "B+ I# I$ -> 5" $
+    eval [] (VBinary OpAdd (VInt 3) (VInt 2)) @?= VInt 5
+  , testCase "B- I$ I# -> 1" $
+    eval [] (VBinary OpSub (VInt 3) (VInt 2)) @?= VInt 1
+  , testCase "B* I$ I# -> 1" $
+    eval [] (VBinary OpMul (VInt 3) (VInt 2)) @?= VInt 6
+  , testCase "B/ U- I( I# -> -3" $
+    eval [] (VBinary OpDiv (VUnary OpNeg (VInt 7)) (VInt 2)) @?= VInt (-3)
+  , testCase "B% U- I( I# -> -3" $
+    eval [] (VBinary OpMod (VUnary OpNeg (VInt 7)) (VInt 2)) @?= VInt (-1)
+  ]
+
+ifTests = testGroup "If eval"
+  []
+
+lambdaTests = testGroup "Lambda eval"
+  []
